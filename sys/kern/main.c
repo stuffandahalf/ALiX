f2a2c939cd4dd6645f5ddee8aaf8cb85f8916a1a
Gregory Norton <gregory.norton@me.com>
2022-04-25 01:27:27 -0400

    Updated configure script to support per target command flags

--- sys/kern/main.c
+++ sys/kern/main.c
@@ -1,5 +1,13 @@
+unsigned char *const framebuffer = (unsigned char *)0xb8000;
+
 void
 main(void)
 {
-
+	const char *str = "Hello kernel world!";
+	const char *c;
+	int i = 0;
+	for (c = str; *c != '\0'; c++) {
+		framebuffer[i++] = *c;
+		framebuffer[i++] = 0xf0;
+	}
 }
--- sys/stub/entry.c
+++
@@ -1,5 +0,0 @@
-void
-_start(void)
-{
-	int a = 1 + 1;
-}
