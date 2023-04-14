3ce7e62aa4535accdc7cae33c8bcd0a72f8c0cd4
Gregory Norton <gregory.norton@me.com>
2023-04-13 23:31:36 -0400

    Removed dead logarithm code

--- sys/arch/generic/log.c
+++
@@ -1,9 +0,0 @@
-#include <arch/log.h>
-
-unsigned long int
-log2(unsigned long int n)
-{
-	unsigned long int ret = 0;
-	while ((n >> ret) > 0) ret++;
-	return ret;
-}
--- sys/arch/x86/log.c
+++
@@ -1,9 +0,0 @@
-#include <arch/log.h>
-
-unsigned long int
-log2(unsigned long int n)
-{
-	unsigned long int ret;
-	__asm__ __volatile__ ("bsrl %1, %0\n\t" : "=r"(ret) : "r"(n));
-	return ret;
-}
